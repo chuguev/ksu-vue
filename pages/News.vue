b946b3e6d163481d517b9ce427ff8bbc27891d53
m.chuguev <m.chuguev@focus.bi>
2021-02-17 17:54:07 +0300

    Выделен компонент новости

---
+++ pages/News.vue
@@ -0,0 +1,37 @@
+<template>
+  <li class="news news-white">
+    <label class="news__date">22 марта</label>
+    <h3 class="news__title">
+      Формируем налоговую культуру: открытая лекция от специалиста ФНС для
+      студентов КГУ
+    </h3>
+    <div class="news__description">
+      В целях популяризации точных наук, информационных технологий и
+      естественно-научных предметов и повышения интереса...
+    </div>
+  </li>
+</template>
+<script>
+export default {
+  name: "News",
+};
+</script>
+<style lang="scss">
+.news {
+  padding: 45px;
+
+  &__date {
+    list-style-type: circle;
+    color: var(--color-font-brown);
+    font-family: Lato;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 10px;
+    line-height: 20px;
+  }
+
+  &__title {
+    font-size: var(--size-font-medium);
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,48 @@
 <template>
-  <section class="container">hello world</section>
+  <section class="grey">
+    <div class="container news-container">
+      <div class="row">
+        <div class="news-container__title">
+          <h2>Лента новостей</h2>
+        </div>
+        <div class="news-container__control">
+          control
+        </div>
+        <ul class="news-container__list">
+          <News />
+        </ul>
+      </div>
+    </div>
+  </section>
 </template>
+
+<style lang="scss">
+.news-container {
+  &__list {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+  }
+}
+
+.news {
+  padding: 45px;
+
+  &__date {
+    list-style-type: circle;
+    color: var(--color-font-brown);
+    font-family: Lato;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 10px;
+    line-height: 20px;
+  }
+}
+</style>
+<script>
+import News from "./News";
+
+export default {
+  components: { News },
+};
+</script>
